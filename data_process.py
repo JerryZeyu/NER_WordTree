1cb3ff1e07bc40209f2d495c67b62c24426dc9d6
JerryZeyu <zzy19951013@gmail.com>
2019-11-18 17:15:20 -0700

    first upload

---
+++ data_process.py
@@ -0,0 +1,6 @@
+import os
+import pandas as pd
+PATH_QUESTIONS = '/home/zeyuzhang/PycharmProjects/ner_wordtree/expl-tablestore-export-2019-09-10-165215'
+PATH_TABLESTORE = '/home/zeyuzhang/PycharmProjects/ner_wordtree/expl-tablestore-export-2019-09-10-165215/tables'
+def divide_questionAnswer:
+    
